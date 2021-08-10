9bc977cc87cea853817542079a413b2263e6837c
Muhammet <muhammetgms95@gmail.com>
2021-08-10 19:31:47 +0300

    Project structure created

---
+++ src/main/java/com/muhammetgumus/fschatapp/FsChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.muhammetgumus.fschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhammetgumus/fschatapp/FsChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammetgumus.fschatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
